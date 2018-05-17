aca6f45e673750ed72dd817fbb83a88bc6ec1913
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-17 08:30:46 +0200

    Fix bugs in timeCalculator.js

--- modules/timeCalculator.js
+++ modules/timeCalculator.js
@@ -1,15 +1,13 @@
-var os = require('os');
 
-function timeCalculator() {
-    var uptime = os.uptime();
-  if (uptime < 60) {
-    console.log('Uptime: ', uptime, "sec");
-  } else if ((uptime >= 60) && (uptime < 3600 )) {
-    console.log('Uptime: ', (uptime / 60).toFixed(0), "min", (uptime % 60), "sec");
-  } else if (uptime >= 3600) {
-    console.log('Uptime: ', (uptime / 3600).toFixed(0), "hour(s)", ((uptime % 3600)/60).toFixed(0), "min", (uptime % 60), "sec");
+
+function timeCalculator(time) {
+  if ( time < 60) {
+    console.log( time, "sec");
+  } else if ( (time >= 60) && (time < 3600 )) {
+    console.log( Math.floor(time / 60), "min", (time % 60), "sec");
+  } else if (time >= 3600) {
+    console.log( Math.floor(time / 3600), "hour(s)", (Math.floor((time % 3600)/60)), "min", (time % 60), "sec");
   }
 }
-
 
 exports.print = timeCalculator;
