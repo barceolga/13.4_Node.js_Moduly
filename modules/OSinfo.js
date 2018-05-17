bafb336f38f6e2c341ff961242c2c5f5b7291eb3
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-17 12:13:35 +0200

    Fix bugs in function getOSinfo for OSinfo.js file

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -16,7 +16,8 @@ function getOSinfo() {
   console.log('System:', type);
   console.log('Release:', release);
   console.log('CPU model:', cpu);
-  console.log(timeInfo.print(uptime));
+  process.stdout.write('Uptime: ');
+  timeInfo.print(uptime);
   console.log('User name:', userInfo.username);
   console.log('Home dir:', userInfo.homedir);
 }
